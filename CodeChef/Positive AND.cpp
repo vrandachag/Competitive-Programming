075dbbcf1cdbbd092bbd0a478c41392762ba1e94
Vranda Chag <65115155+vrandachag@users.noreply.github.com>
2020-10-14 22:22:39 +0530

    Add files via upload

---
+++ CodeChef/Positive AND.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+    unsigned int T,N,;
+    cin>>T;
+    while(T--)
+    {
+        cin>>N;
+        if(N==1)
+            cout<<1<<"\n";
+        else if(N&N-1 == 0)
+            cout<<-1<<"\n";
+        else
+        {
+            for(int i=2;i<N;i++)
+            {
+                cout<<i<<"\t"<<i-1;
+            }
+        }
+    }
+	return 0;
+}
