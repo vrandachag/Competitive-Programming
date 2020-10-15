4c79b06afa68ae61eebf9414cce62dcfa50242e9
Vranda Chag <65115155+vrandachag@users.noreply.github.com>
2020-10-15 14:39:13 +0530

    Update Positive AND.cpp

--- CodeChef/Positive AND.cpp
+++ CodeChef/Positive AND.cpp
@@ -4,22 +4,39 @@ using namespace std;
 
 int main() 
 {
-    unsigned int T,N,;
+    long int N,count=1;
+    int T;
     cin>>T;
     while(T--)
     {
+        count=1;
         cin>>N;
         if(N==1)
-            cout<<1<<"\n";
-        else if(N&N-1 == 0)
-            cout<<-1<<"\n";
+            cout<<"1";
+        else if((N&N-1) == 0)
+            cout<<"-1";
         else
         {
-            for(int i=2;i<N;i++)
+            cout<<2<<" "<<3<<" "<<1<<" ";
+            for(int i=5;i<=N;)
             {
-                cout<<i<<"\t"<<i-1;
+                if(count==1)
+                {    
+                    cout<<i<<" "<<i-1<<" ";
+                    i=i+2;
+                    count=0;
+                }
+                else
+                {
+                    cout<<i-1<<" "<<i<<" ";
+                    i=i+2;
+                    count=1;
+                }
             }
+             if(N%2==0)
+                cout<<N;
         }
+        cout<<"\n";
     }
 	return 0;
 }
