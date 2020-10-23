c6d0474b574b8bd457b109e957e1b5d054b4faec
Vranda Chag <65115155+vrandachag@users.noreply.github.com>
2020-10-23 17:44:52 +0530

    Update Replace for X_REPLESX.cpp

--- CodeChef/Replace for X_REPLESX.cpp
+++ CodeChef/Replace for X_REPLESX.cpp
@@ -6,40 +6,36 @@ using namespace std;
 
 int main() 
 {
-    int T,N,X,P,K,arr,count=0;
-    vector<int>a;
-    vector<int>:: iterator low,upper;
+    long long int T,N,X,P,K,arr;
+    vector<long long int>:: iterator low,upper;
     cin>>T;
     while(T--)
     {
-        count=0;
         cin>>N>>X>>P>>K;
+         vector<long long int>a;
         for(int i=0;i<N;i++)
         {
             cin>>arr;
             a.push_back(arr);
         }
-	
+        
 		sort(a.begin(),a.end());
-		low=lower_bound(a.begin(),a.end(),X);
-		upper=upper_bound(a.begin(),a.end(),X);
-	
+		
 	    if(a[P-1]==X)
 	        cout<<0;
-	        
-        if(K>P)
+        else if(K>=P && (a[P-1]>X))
 		{
-			if(a[P-1]>X)
-			    cout<<P-upper;
-			else
-			
+		    upper=upper_bound(a.begin(),a.end(),X);
+		    cout<<(P-(upper-a.begin()));
 		}
-		else
-        {
-                
-        }
+		else if(K<=P && (a[P-1]<X))
+		{
+		    low=lower_bound(a.begin(),a.end(),X);
+		    cout<<(low-a.begin())-P+1;
+		}
+        else
+            cout<<"-1";
         cout<<"\n";
-        a.clear();
     }
 	return 0;
 }
