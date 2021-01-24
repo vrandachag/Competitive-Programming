b87889de84a3d3c270240e7442f6fe4e2e89fae7
Vranda Chag <65115155+vrandachag@users.noreply.github.com>
2021-01-25 01:09:26 +0530

    Update MANYSUMS.cpp

--- CodeChef/MANYSUMS.cpp
+++ CodeChef/MANYSUMS.cpp
@@ -28,3 +28,28 @@ int main()
     }
 	return 0;
 }
+
+/*
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    unsigned int L,R,T;
+    cin>>T;
+    while(T--)
+    {
+        cin>>L>>R;
+        unsigned int count = 0;
+        if(L == R)
+            count = 1;
+        else if(R>L)
+        {
+            unsigned int diff=R-L+1;
+            count = diff + (((diff)*(diff-1))/2);
+        }
+        cout<<count<<"\n";
+    }
+	return 0;
+}
+*/
