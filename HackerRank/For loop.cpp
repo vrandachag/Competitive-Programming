42bbb8e1bcc0e7c1b13d6a7bdbbeed05b4b60e93
Vranda Chag <65115155+vrandachag@users.noreply.github.com>
2021-01-27 01:16:23 +0530

    Create For loop.cpp

---
+++ HackerRank/For loop.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() 
+{
+    int a,b;
+    char numbers[][10] = {"one","two","three","four","five","six","seven","eight","nine"};
+    cin>>a;
+    cin>>b;
+    for(int i=a;i<=b;i++)
+    {
+        if(i==a || (i == b && b<=9))
+            cout<<numbers[i-1]<<"\n";
+        else if(i == b && b > 9)
+            cout<<"nine\n";
+    }
+    if(a%2 == 0)
+        cout<<"even\n";
+    else
+        cout<<"odd\n";
+    if(b%2 == 0)
+        cout<<"even\n";
+    else
+        cout<<"odd\n";
+
+    return 0;
+}
