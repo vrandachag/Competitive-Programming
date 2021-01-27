9fe9d8efa9af1407468046ddfcd6194d453d7fd5
Vranda Chag <65115155+vrandachag@users.noreply.github.com>
2021-01-27 10:59:51 +0530

    Create Stdin_Stdout.java

---
+++ HackerRank/Java/Stdin_Stdout.java
@@ -0,0 +1,17 @@
+//https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem?h_r=next-challenge&h_v=zen
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) 
+    {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
