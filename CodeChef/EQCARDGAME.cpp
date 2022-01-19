ba6b4fce7d920ad4456e81ae6dd28a2d646acc6b
Vranda Chag <65115155+vrandachag@users.noreply.github.com>
2022-01-19 23:01:55 +0530

    Create EQCARDGAME.cpp

---
+++ CodeChef/EQCARDGAME.cpp
@@ -0,0 +1,15 @@
+//https://www.codechef.com/START22C/problems/EQCARDGAME
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T,k;
+    cin>>T;
+    while(T--)
+    {
+        cin>>k;
+        cout<<(52%k)<<endl;
+    }
+	// your code goes here
+	return 0;
+}
